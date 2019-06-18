eb15105e1eb2b6e83131de11d0a07cc1f79c9775
zhixilu <321420745@qq.com>
2019-06-18 17:44:17 +0800

    refactor: 💡 edit note.service

--- apps/api/src/app/services/note.service.ts
+++ apps/api/src/app/services/note.service.ts
@@ -3,7 +3,6 @@ import { Injectable } from '@nestjs/common';
 import { Note } from '@magnus/db';
 @Injectable()
 export class NoteService {
-<<<<<<< HEAD
 	constructor(
 		public readonly client: MagnusClient
 	) { }
@@ -39,49 +38,6 @@ export class NoteService {
 			}
 		})
 	}
-=======
-    constructor(
-        public readonly client: MagnusClient
-    ) { }
-    /**
-     * 添加笔记
-     */
-    async NoteSave(note: Note): Promise<Note> {
-        return await this.client.mutate({
-            mutation: gql`
-             mutation NoteSave($entity:NoteInput!,$options:SaveOptions){
-               noteSave(entity:$entity,options:$options){
-                 note_id,
-                 title,
-                 content,
-                 create_time,
-                 update_time,
-                 labels{
-                    label_id,
-                    name,
-                    create_time,
-                    update_time
-      
-    }
-  }
-}
-            `,
-            variables: {
-                "entity": {
-                    "title": note.title,
-                    "content": note.content,
-                    "labels": note.labels
-                }
-            }
-        })
-
-        
-    }
->>>>>>> e1ba06e03e90a67c1273d2b928518d088bc074f9
-
-
-
-
 
 }
 
