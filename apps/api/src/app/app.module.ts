7b39e6af00bcc3157eeeafeb6b2a2cf0ffa4a8ec
zhixilu <321420745@qq.com>
2019-06-18 14:49:48 +0800

    feat: 🎸 add magnus-client

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
-
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { MagnusClientModule } from './magnusClient';
 @Module({
-  imports: [],
+  imports: [
+    MagnusClientModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
--- apps/api/src/app/magnusClient.ts
+++ apps/api/src/app/magnusClient.ts
@@ -4,7 +4,7 @@ import { join } from 'path';
 
 @Module({})
 export class MagnusClientModule {
-    static forRoot(path: string, uri: string = 'http://localhost:3333/api'): DynamicModule {
+    static forRoot(path: string, uri: string = 'http://localhost:3300'): DynamicModule {
         return NotaddMagnusClient.create({
             inputGolb: join(path, '**/*.ts'),
             outputPath: path,
