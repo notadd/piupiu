d64faa59b04402d9d2dfc01e37e91a133d317cd6
zhixilu <321420745@qq.com>
2019-06-18 16:07:05 +0800

    refactor: 💡 edit magnus-cli

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -2,11 +2,19 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MagnusClientModule } from './magnusClient';
+import { NoteController } from './controller/note.controller';
+import { NoteService } from './services/note.service';
 @Module({
   imports: [
-    MagnusClientModule
+    MagnusClientModule.forRoot(__dirname)
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [
+    AppController,
+    NoteController
+  ],
+  providers: [
+    AppService,
+    NoteService
+  ],
 })
 export class AppModule { }
--- apps/api/src/app/controller/note.controller.ts
+++ apps/api/src/app/controller/note.controller.ts
@@ -5,8 +5,10 @@ import { Note } from 'libs/db/entitys/note.entity';
 @Controller()
 export class NoteController{
     constructor(@Inject(NoteService) public readonly noteService: NoteService) { }
+    
     @Post('save')
     async NoteSave(@Body() note:Note):Promise<any>{
+        console.log(note)
         return await this.noteService.NoteSave(note);
     }
 }
