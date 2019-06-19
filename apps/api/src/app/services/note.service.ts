93273f5fb0707c147c9fb0edf7497293589ce249
zhixilu <321420745@qq.com>
2019-06-19 10:40:15 +0800

    refactor: 💡 edit label.service

--- apps/api/src/app/services/note.service.ts
+++ apps/api/src/app/services/note.service.ts
@@ -1,11 +1,13 @@
 import { MagnusClient, gql } from '@notadd/magnus-client';
 import { Injectable } from '@nestjs/common';
 import { Note } from '@magnus/db';
+import { LabelService } from './label.service';
 
 @Injectable()
 export class NoteService {
 	constructor(
-		public readonly client: MagnusClient
+		public readonly client: MagnusClient,
+		public readonly labelService: LabelService
 	) { }
 
 	/**
@@ -16,16 +18,9 @@ export class NoteService {
 			mutation: gql`
             mutation NoteSave($entity:NoteInput!,$options:SaveOptions){
                 noteSave(entity:$entity,options:$options){
-                	note_id,
-                 	title,
-                 	content,
-                 	create_time,
-                 	update_time,
-                 	labels{
-						label_id,
-						name,
-						create_time,
-						update_time
+					note_id, title, content, create_time, update_time, 
+					labels{
+						label_id, name, create_time, update_time
            		 	}
          	 	}
         	}
@@ -39,8 +34,34 @@ export class NoteService {
 			}
 		})
 	}
+
 	/**
-	 * 
+	 * 修改笔记
+	 */
+	async updateNote(note: Note): Promise<Note> {
+		return await this.client.mutate({
+			mutation: gql`
+            mutation NoteSave($entity:NoteInput!,$options:SaveOptions){
+                noteSave(entity:$entity,options:$options){
+					note_id, title, content, create_time, update_time, 
+					labels{
+						label_id, name, create_time, update_time
+           		 	}
+         	 	}
+        	}
+            `,
+			variables: {
+				"entity": {
+					"note_id": note.note_id,
+					"title": note.title,
+					"content": note.content,
+					"labels": note.labels
+				}
+			}
+		})
+	}
+	
+	/**
 	 * @param where 根据笔记id查询
 	 */
 	async NoteFindOne(where: Partial<Note>): Promise<Note> {
@@ -60,15 +81,15 @@ export class NoteService {
     }
 }
 			`,
-			variables:{
+			variables: {
 				"options": {
 					"where": {
-					  "note_id": where.note_id
+						"note_id": where.note_id
 					}
+				}
 			}
-		}
 		});
 		return result.data;
-	
+
 	}
 }
